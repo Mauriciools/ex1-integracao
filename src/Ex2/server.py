2f50435fa873a0e406f6326ffb2d79ad8808f425
Mauricio Salvador <mauricio.salvador@gemineers.com>
2024-04-22 22:13:38 +0200

    Server function to send the sum of two numbers to the html client

--- src/Ex2/server.py
+++ src/Ex2/server.py
@@ -51,8 +51,49 @@ async def time(websocket, path):
         message = await websocket.recv()
         await websocket.send("Received final message!")
 
+# add_numbers function to send the result of the addition of two numbers to the client
+async def add_numbers(websocket, path):
+    while True:
+        try:
+            print("\n")
+            
+            # Ask the user for the first number to be added and send to the server
+            num1 = float(input("Enter the first number to be added: "))
+            
+            # Ask the user for the second number to be added and send to the server
+            num2 = float(input("Enter the second number to be added: "))
+
+            # Verify if the numbers are int or float
+            if (isinstance(num1, (int, float)) and isinstance(num2, (int, float))):
+                # Send to the client a message containing the sum of both passed numbers
+                await websocket.send(f"{num1} + {num2} = {num1 + num2}")
+
+                # Random sleep
+                await asyncio.sleep(random.random() * 3)
+            else:
+                # If the arguments are not int or float, send back to the client an error message
+                await websocket.send("One of the arguments is not valid, please, enter only numbers.")
+        except:
+            # If any error occurred during operation, send back to the client an error message
+            await websocket.send("One of the arguments is not valid, please, enter only numbers.")
+
+        # Ask the user whether the program should continue or not. Only Y or N valid
+        message = ""
+        while ((len(message) == 0)):
+            message = input("Do you wish to continue? (Yes / No) ")
+            message = message.upper().strip()[0]
+            
+            # If the answer is different from Y (yes) or N (no)
+            # set the message to empty so the program asks the user again
+            if ((message != 'Y') and (message != 'N')):
+                message = ""
+
+        # If the user doesn't want to continue, break out of the loop
+        if (message == 'N'):
+            break
+
 # Start the server on the specified address and port
-start_server = websockets.serve(time, '127.0.0.1', 5678)
+start_server = websockets.serve(add_numbers, '127.0.0.1', 5678)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
