40c3918d4be6ea0a7e02dfebd0b5a67558df743f
Mauricio Salvador <mauricio.salvador@gemineers.com>
2024-04-25 22:20:49 +0200

    Comments to write and read files

--- src/Ex3/read.py
+++ src/Ex3/read.py
@@ -1,40 +1,54 @@
+#Import modules/libs
 import sqlite3
 
+# Connect to a .db file
 conn = sqlite3.connect('aula.db')
 c = conn.cursor()
 
+# Function to print on screen the received data
 def print_data(data):
     for row in data:
         print(row)
 
+# Function that executes multiple queries to the database
 def read_from_db():
+    # Retrieve all students and their information
     print("Todos os alunos:")
     c.execute("SELECT * FROM Alunos")
     print_data(c.fetchall())
 
+    # Retrieve all ages from all students
     print("\nTodas as idades:")
     c.execute("SELECT Idade FROM Alunos WHERE Idade > 18")
     print_data(c.fetchall())
 
+    # Retrieve who has 'frozen' the course
     print("\nQuem trancou o curso?")
     c.execute("SELECT Nome FROM Alunos WHERE Situacao = 'Trancamento'")
     print_data(c.fetchall())
 
+    # Retrieve who entered after 2012
     print("\nQuem entrou após 2012?")
     c.execute("SELECT Nome FROM Alunos WHERE Ingresso > 2012")
     print_data(c.fetchall())
 
+    # Retrieve Patrick's age
     print("\nQual a idade de Patrick?")
     c.execute("SELECT Idade FROM Alunos WHERE Nome = 'Patrick'")
     print_data(c.fetchall())
 
+    # Update the name of Pedro to Carlos
     print("\nMudando o nome de Pedro para Carlos...")
     c.execute("UPDATE Alunos SET Nome = 'Carlos' WHERE Nome = 'Pedro'")
 
+    # Retrieve all students and their information (updated)
     print("Todos os alunos (atualizado):")
     c.execute("SELECT * FROM Alunos")
     print_data(c.fetchall())
 
+# Execute all queries
 read_from_db()
+
+# Close the connection
 c.close()
 conn.close()
--- src/Ex3/write.py
+++ src/Ex3/write.py
@@ -1,13 +1,17 @@
+# Import modules/libs
 import sqlite3
 
+# Create and connect to a .db file
 conn = sqlite3.connect('aula.db')
 c = conn.cursor()
 
+# Function to create the 'Alunos' table, if not existent
 def create_table():
     c.execute(
         "CREATE TABLE IF NOT EXISTS Alunos(Nome TEXT, Matricula REAL, Idade INTEGER, Ingresso INTEGER, Situacao TEXT)"
     )
 
+# Function to populate the 'Alunos' table with some data
 def data_entry():
     c.execute(
         """INSERT INTO Alunos(Nome, Matricula, Idade, Ingresso, Situacao) VALUES 
@@ -17,9 +21,11 @@ def data_entry():
             ('Patrick', 14204123, 24, 2022, 'Desistiu');"""
     )
 
+# Create table and populate it
 create_table()
 data_entry()
 
+# Commit and close the connection
 conn.commit()
 c.close()
 conn.close()
