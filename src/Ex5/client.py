a9cd798e851ef024f09e18fe57e4e3217d61cec1
Mauricio Salvador <mauricio.salvador@gemineers.com>
2024-05-01 14:24:05 +0200

    Server and client for FTP application and file updates

--- src/Ex5/client.py
+++ src/Ex5/client.py
@@ -1,26 +1,70 @@
+# Import modules/libs
+import time
 import socket
+import datetime
 
+# Define host and port
 host = "localhost"
 port = 6001
 
+# Connect to the server using the address
 s = socket.socket()
 s.connect((host, port))
+s.setblocking(0)
 
-with open("test.xlsx", 'wb') as f:
+# Open specified file as write binary mode
+with open("FT_file_received.txt", 'wb') as f:
     print("File opened.")
 
+    time.sleep(1)
+
     while (True):
-        print("Receiving data...")
-        data = s.recv(1024 * 16)
+        print("Receiving data from server...")
 
-        if not data:
-            break
+        # Try to receive data from the server
+        try:
+            data = s.recv(1024 * 16)
+
+            # If there is no data just goes out of the while loop
+            if not data:
+                break
         
-        f.write(data)
+            # Write existing data to file
+            f.write(data)
+        except:
+            break      
 
+    print("File successfully received!")
+
+    print("\nUpdating file now...")
+
+    # Define an update string and append to file
+    updatedStr = f"\nUpdated by the client at {datetime.datetime.now()}"
+    f.write(updatedStr.encode('utf-8'))
+
+    # Close file
     f.close()
-    print("File successfully transfered!")
+    print("File successfully updated!")
 
-    s.close()
-    print("connection closed.")
+# Delay between updating file and sending back data to the server
+time.sleep(1)
     
+# Open specified file as read binary mode
+with open("FT_file_received.txt", 'rb') as f:
+    print("\nSending back data to the server...")
+
+    # Read file content
+    l = f.read(1024 * 16)
+
+    # Iterates over the entire file content and send the corresponding data to the server
+    while (l):
+        s.send(l)
+        l = f.read(1024 * 16)
+
+    # Close file
+    f.close()
+    print("Updated file successfully sent back to the server!")
+
+    # Close connection with the server
+    s.close()
+    print("\nConnection closed.")
--- src/Ex5/server.py
+++ src/Ex5/server.py
@@ -1,26 +1,78 @@
+# Import modules/libs
+import time
 import socket
+import datetime
 
+# Define host and port
 host = "localhost"
 port = 6001
 
+# Configure server settings using socket
 s = socket.socket()
 s.bind((host, port))
 s.listen(1)
+s.setblocking(0)
 
 while (True):
-    connection, address = s.accept()
-    print("Connected by:", address)
+    # Try to accept a new client connection
+    try:
+        connection, address = s.accept()
+        print("Connected by:", address)
+    except:
+        # Otherwise just skips to the next iteration
+        continue
 
-    filename = "test.xlsx"
+    print("\nSending file to client...")
+
+    # Open specified file as read binary mode
+    filename = "FT_test.txt"
     f = open(filename, 'rb')
+
+    # Read file content
     l = f.read(1024 * 16)
 
+    # Iterates over the entire file content and send the corresponding data to the client
     while (l):
         connection.send(l)
         l = f.read(1024 * 16)
 
+    # Close file
     f.close()
-    print("File sent!")
+    print("File successfully sent!")
+
+    # Wait time for file changes on client side
+    time.sleep(5)
+
+    print("\nUpdating server's root file now...")
+
+    # Open specified file as write binary mode
+    f = open(filename, 'wb')
+    print("File opened.")
+
+    while (True):
+        print("Receiving data from client...")
+
+        # Try to receive data back from the client
+        try:
+            data = connection.recv(1024 * 16)
+
+            # If there is no data just goes out of the while loop
+            if not data:
+                break
+        
+            # Write existing data to file
+            f.write(data)
+        except:
+            break
+
+    # Define an update string and append to file
+    updatedStr = f"\nFile successfully updated in the server at {datetime.datetime.now()}"
+    f.write(updatedStr.encode('utf-8'))
     
+    # Close file
+    f.close()
+    print("File successfully updated on server side!")
+    
+    # Close connection with the client
     connection.close()
-    print("Connection closed.")
+    print("\nConnection closed.\n")
